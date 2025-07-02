1c402e544fda828685b110924a97ae782cdc0996
Asad Ali <113229922+asadali2004@users.noreply.github.com>
2025-07-02 15:35:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 
 const requestHandler = (req, res) => {
-  res.end('Hello from Node.js App!');
+  res.end('Hello from Node.js App! this is asad');
 };
 
 const server = http.createServer(requestHandler);
